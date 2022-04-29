eb7451d5050e838a148ae483b1ff0e3bdcf948f7
Franpanozzo <francisco.panozzosf@gmail.com>
2022-04-29 16:13:17 -0300

    time and count

--- appV5-2.js
+++ appV5-2.js
@@ -1,26 +1,54 @@
 const path = require('path');
 const fs = require('fs');
 const { performanceFunc } = require('./appV5');
+const util = require('util');
+
+const log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+const log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
 
 const directoryPath = path.join(__dirname, 'single-pdf');
+const configuration = [{r: 300, scaleFactor: 3, processedPdfs: 0}, {r: 600, scaleFactor: 2,  processedPdfs: 0}, {r: 1200, scaleFactor: 3,  processedPdfs: 0}];
 
 async function func() {
+  let pdfConQrNoProcesados = [];
   fs.readdir(directoryPath, async function (err, files) {
     if (err) {
           return console.log('Unable to scan directory: ' + err);
       } 
-
+      // console.time("Tiempo");
+      const begin =Date.now();
       for (const file of files) {
         console.log(`El path del pdf ${file}: ${path.join(directoryPath,file)}`); 
-          const { qrCode, config } = await performanceFunc(directoryPath, file);
+          const pdfInfo = await performanceFunc(directoryPath, file, configuration);
 
-          if(qrCode) {
-            console.log('QR CODE:', qrCode);
-            console.log('CONFIG:', config);
+          if(!pdfInfo.qrCode) {
+            pdfConQrNoProcesados.push(file);
           }
-          else console.log('No AFIP QR code for,',file);
+          // else pdfConQrNoProcesados.push(file);
       }
+      const end = Date.now();
+
+      const timeSpent =(end-begin)/1000+" secs";
+      console.log(`Time: ${timeSpent}`);
+      // console.timeEnd("Tiempo");
+      console.log(configuration);
+      console.log(`PDFs with no QR CODE: ${pdfConQrNoProcesados.length}`);
+      console.log(pdfConQrNoProcesados);
+      // console.log('PDFs WITH QR CODES: ');
+      // for (const pdf of pdfConQrProcesados) {
+      //   console.log(pdf)
+      // }
+      // console.log('PDFs WITHOUT QR CODES: ');
+      // for (const pdf of pdfConQrNoProcesados) {
+      //   console.log(pdf)
+      // }
   });
+
 }
 
 func();
--- appV5.js
+++ appV5.js
@@ -20,7 +20,7 @@ function convertPdfToImage(conf, pdfFilePath, imagePathMultiPage) {
       .input(pdfFilePath)
       .exec((err, stdout, stderr) => {
         if (!err) {
-          console.log('GS executed successfully! PDF PATH:', pdfFilePath);
+          console.log('GS executed successfully!');
           // console.log('stdout', stdout)
           // console.log('stderr', stderr)
           resolve(pdfFilePath)
@@ -57,13 +57,14 @@ function getQrFromPdf(config, directoryPath, pdf) {
   // const PDF_NAME = 'sample10';
 
   const pdfFilePath = path.join(directoryPath, pdf);
-  pdfName = path.basename(pdfFilePath, '.pdf')
+  const pdfName = path.basename(pdfFilePath, '.pdf')
   const imagePathMultiPage = path.join(__dirname ,'images',`${pdfName}-%03d.png`);
 
   try {
     return convertPdfToImage(config, pdfFilePath, imagePathMultiPage)
       .then(res => {
-        return readQrFromImage(pdfName);
+        const coso = readQrFromImage(pdfName);
+        return coso;
       });
   } catch(err) {
     console.log('Failed to convert PDF to image -',err);
@@ -71,26 +72,27 @@ function getQrFromPdf(config, directoryPath, pdf) {
   }
 }
 
-async function performanceFunc(directoryPath, file) {
-  const configuration = [{r: 300, scaleFactor: 3}, {r: 600, scaleFactor: 2}, {r: 1200, scaleFactor: 3}];
+async function performanceFunc(directoryPath, file, configuration) {
+  // const configuration = [{r: 300, scaleFactor: 3}, {r: 600, scaleFactor: 2}, {r: 1200, scaleFactor: 3}];
   let iterator = 0;
   let qrCode = undefined, config;
 
   while(config = configuration[iterator]) {
-    console.log(`GETTING QR CODE WITH - r:${config.r}, scaleFactor:${config.scaleFactor}`);
     qrCode = await getQrFromPdf(config, directoryPath, file);
-    if(qrCode) break
+    if(qrCode) {
+      config.processedPdfs++;
+      break
+    }
     iterator++;
     if(!config) {
       break;
     }
   }
 
-  if(qrCode) return {
+  return {
     qrCode,
     config
   };
-  else return null;
 }
 
 
